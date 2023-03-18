93b2e0a048ff08a3f3770efe97bda76e9af92bc0
Karan Bagle <karanbagle420@GMAIL.COM>
2023-03-18 01:52:42 -0700

    k

---
+++ lib/utils/contact_utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContactUtils {
+  static const List<IconData> contactIcon = [
+    Icons.home,
+    Icons.phone,
+    Icons.mail,
+  ];
+
+  static const List<String> titles = [
+    "Location",
+    "Phone",
+    "Email",
+  ];
+
+  static const List<String> details = [
+    "Hayward, California",
+    "(+1) 510 696 0144",
+    "karanbagle01@gmail.com",
+  ];
+}
