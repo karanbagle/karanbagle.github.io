dd5297cf34148d3826cf3f1ec98c9d7dbd355b0e
Karan Bagle <karanbagle420@GMAIL.COM>
2023-03-18 00:42:05 -0700

    m

--- devfolio-main/lib/utils/about_utils.dart
+++
@@ -1,7 +0,0 @@
-class AboutUtils {
-  static const String aboutMeHeadline =
-      'I\'m Karan Bagle, a Flutter developer, Technical blog writer and UI designer.';
-
-  static const String aboutMeDetail =
-      'I am currently a Master\'s of Science in Computer Science student at California State University, East Bay in Hayward, CA and am interested in pursuing a Summer 2023 SWE Internship. My expected graduation date is May 2024. I am particularly interested in Frontend development, Blockchain development, development of micro-services, and distributed systems. I have experience working on projects and conducting research in the areas of Enterprise Applications and Decentralized Systems. I bring with me two and a half years of work experience from big companies like Accenture and Tech Mahindra, where I worked with Javascript, C++, Android, .Net, Full Stack Testing, and Salesforce. Additionally, I have expertise in stacks such as AWS, Git, Jira, EC2, GraphQL, Microservices, Kafka, Postman, Truffle, and proficiency with databases like SQL, MongoDB, Amazon RDS, Postgres, and Firebase. I am passionate about using technology to provide solutions to complex problems and am eager to collaborate with like-minded individuals and learn from industry experts.';
-}
